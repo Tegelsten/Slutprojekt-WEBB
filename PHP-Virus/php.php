3cb8fb90e9bca1df18f1a6ffceeac4702fd9e4c4
Tegelsten <etagebrand@hotmail.com>
2016-04-07 12:44:06 +0200

    pp1rq
    1ff

--- PHP-Virus/php.php
+++ PHP-Virus/php.php
@@ -9,14 +9,14 @@
 		
 		require_once('phpsaurus.php');
 		
-		$query = "SELECT ID, Liga FROM uuuu";
+		$query = "SELECT ID, FaktaNamn, FaktaLank, Roster FROM unFakta";
 	
 		$result = mysqli_query($dbc, $query);
 		
 		if (mysqli_num_rows($result) > 0) {
-		// output data of each row
+		// Skriver ut datan från varje rad
 		while($row = mysqli_fetch_assoc($result)) {
-				echo "ID: " . $row["ID"]. " - Liga: " . $row["Liga"]. "<br>";
+				echo "ID: " . $row["ID"]. " - Faktanamn: " . $row["FaktaNamn"]. " - Faktalänk: " . $row["FaktaLank"]. " - Röster: " . $row["Roster"]. "<br>";
 			}
 		} else {
 			echo "0 results";
