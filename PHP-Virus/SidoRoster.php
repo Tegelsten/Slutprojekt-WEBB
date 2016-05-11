be2a90efd458d1debc5b10c121ba5c1f6b619df5
Tegelsten <etagebrand@hotmail.com>
2016-05-11 10:33:39 +0200

    virtustrojsn

--- PHP-Virus/SidoRoster.php
+++ PHP-Virus/SidoRoster.php
@@ -6,7 +6,34 @@
 </head>
 	
     <?php
-		echo "ssss";
+		
+		include ("phpsaurus.php");
+	
+		$query = "SELECT `Sida1`, `Roster1`, `Sida2`, `Roster2` FROM unversus LIMIT 1";
+
+		$response = @mysqli_query($dbc, $query);
+		
+		if($response){
+			
+			while($row = mysqli_fetch_array($response)){
+		
+				$Sida1 = $row['Sida1'];
+				
+				$Roster1 = $row['Roster1'];
+				
+				$Sida2 = $row['Sida2'];
+				
+				$Roster2 = $row['Roster2'];
+			
+			}
+			
+		} else {
+	
+				echo "Nope";
+				echo mysqli_error($dbc);
+
+		}
+		
 	?>
     
 <body>
